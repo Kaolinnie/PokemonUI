fb4d54e087ca9a9df14072df421516d9caa8b89f
MercyWhistles <82103220+ErisWhistles@users.noreply.github.com>
2022-12-07 14:58:09 -0500

    label for lang

--- PokemonUI/PokemonUIForm.Designer.cs
+++ PokemonUI/PokemonUIForm.Designer.cs
@@ -32,20 +32,21 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PokemonUIForm));
             this.menuPanel = new System.Windows.Forms.Panel();
             this.buttonsPanel = new System.Windows.Forms.Panel();
-            this.icons = new System.Windows.Forms.ImageList(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            this.logoPanel = new System.Windows.Forms.Panel();
-            this.appTitleLabel = new System.Windows.Forms.Label();
-            this.menuTitlePanel = new System.Windows.Forms.Panel();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.menuTitleLabel = new System.Windows.Forms.Label();
-            this.desktopPanel = new System.Windows.Forms.Panel();
-            this.types = new System.Windows.Forms.ImageList(this.components);
             this.settingsButton = new System.Windows.Forms.Button();
+            this.icons = new System.Windows.Forms.ImageList(this.components);
             this.pokemonButton = new System.Windows.Forms.Button();
             this.itemsButton = new System.Windows.Forms.Button();
             this.movesButton = new System.Windows.Forms.Button();
             this.teamsButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.logoPanel = new System.Windows.Forms.Panel();
+            this.appTitleLabel = new System.Windows.Forms.Label();
+            this.menuTitlePanel = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuTitleLabel = new System.Windows.Forms.Label();
+            this.desktopPanel = new System.Windows.Forms.Panel();
+            this.types = new System.Windows.Forms.ImageList(this.components);
             this.menuPanel.SuspendLayout();
             this.buttonsPanel.SuspendLayout();
             this.logoPanel.SuspendLayout();
@@ -72,6 +73,15 @@
             this.buttonsPanel.Controls.Add(this.teamsButton);
             this.buttonsPanel.Name = "buttonsPanel";
             // 
+            // settingsButton
+            // 
+            resources.ApplyResources(this.settingsButton, "settingsButton");
+            this.settingsButton.FlatAppearance.BorderSize = 0;
+            this.settingsButton.ImageList = this.icons;
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.UseVisualStyleBackColor = true;
+            this.settingsButton.Click += new System.EventHandler(this.settingsButton_Click);
+            // 
             // icons
             // 
             this.icons.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("icons.ImageStream")));
@@ -83,68 +93,6 @@
             this.icons.Images.SetKeyName(4, "1337524_game_go_moltres_play_pokemon_icon.png");
             this.icons.Images.SetKeyName(5, "icons8-pokedex-96.png");
             this.icons.Images.SetKeyName(6, "icons8-pokebag-96.png");
-            // 
-            // label1
-            // 
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.Name = "label1";
-            // 
-            // logoPanel
-            // 
-            resources.ApplyResources(this.logoPanel, "logoPanel");
-            this.logoPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
-            this.logoPanel.Controls.Add(this.appTitleLabel);
-            this.logoPanel.Name = "logoPanel";
-            // 
-            // appTitleLabel
-            // 
-            resources.ApplyResources(this.appTitleLabel, "appTitleLabel");
-            this.appTitleLabel.Name = "appTitleLabel";
-            // 
-            // menuTitlePanel
-            // 
-            resources.ApplyResources(this.menuTitlePanel, "menuTitlePanel");
-            this.menuTitlePanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.menuTitlePanel.Controls.Add(this.comboBox1);
-            this.menuTitlePanel.Controls.Add(this.menuTitleLabel);
-            this.menuTitlePanel.Name = "menuTitlePanel";
-            // 
-            // comboBox1
-            // 
-            resources.ApplyResources(this.comboBox1, "comboBox1");
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            resources.GetString("comboBox1.Items"),
-            resources.GetString("comboBox1.Items1"),
-            resources.GetString("comboBox1.Items2")});
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
-            // menuTitleLabel
-            // 
-            resources.ApplyResources(this.menuTitleLabel, "menuTitleLabel");
-            this.menuTitleLabel.ForeColor = System.Drawing.Color.Gainsboro;
-            this.menuTitleLabel.Name = "menuTitleLabel";
-            // 
-            // desktopPanel
-            // 
-            resources.ApplyResources(this.desktopPanel, "desktopPanel");
-            this.desktopPanel.Name = "desktopPanel";
-            // 
-            // types
-            // 
-            this.types.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
-            resources.ApplyResources(this.types, "types");
-            this.types.TransparentColor = System.Drawing.Color.Transparent;
-            // 
-            // settingsButton
-            // 
-            resources.ApplyResources(this.settingsButton, "settingsButton");
-            this.settingsButton.FlatAppearance.BorderSize = 0;
-            this.settingsButton.ImageList = this.icons;
-            this.settingsButton.Name = "settingsButton";
-            this.settingsButton.UseVisualStyleBackColor = true;
-            this.settingsButton.Click += new System.EventHandler(this.settingsButton_Click);
             // 
             // pokemonButton
             // 
@@ -182,6 +130,65 @@
             this.teamsButton.UseVisualStyleBackColor = true;
             this.teamsButton.Click += new System.EventHandler(this.teamsButton_Click);
             // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // logoPanel
+            // 
+            resources.ApplyResources(this.logoPanel, "logoPanel");
+            this.logoPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.logoPanel.Controls.Add(this.appTitleLabel);
+            this.logoPanel.Name = "logoPanel";
+            // 
+            // appTitleLabel
+            // 
+            resources.ApplyResources(this.appTitleLabel, "appTitleLabel");
+            this.appTitleLabel.Name = "appTitleLabel";
+            // 
+            // menuTitlePanel
+            // 
+            resources.ApplyResources(this.menuTitlePanel, "menuTitlePanel");
+            this.menuTitlePanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.menuTitlePanel.Controls.Add(this.label2);
+            this.menuTitlePanel.Controls.Add(this.comboBox1);
+            this.menuTitlePanel.Controls.Add(this.menuTitleLabel);
+            this.menuTitlePanel.Name = "menuTitlePanel";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2")});
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // menuTitleLabel
+            // 
+            resources.ApplyResources(this.menuTitleLabel, "menuTitleLabel");
+            this.menuTitleLabel.ForeColor = System.Drawing.Color.Gainsboro;
+            this.menuTitleLabel.Name = "menuTitleLabel";
+            // 
+            // desktopPanel
+            // 
+            resources.ApplyResources(this.desktopPanel, "desktopPanel");
+            this.desktopPanel.Name = "desktopPanel";
+            // 
+            // types
+            // 
+            this.types.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            resources.ApplyResources(this.types, "types");
+            this.types.TransparentColor = System.Drawing.Color.Transparent;
+            // 
             // PokemonUIForm
             // 
             resources.ApplyResources(this, "$this");
@@ -196,6 +203,7 @@
             this.buttonsPanel.ResumeLayout(false);
             this.logoPanel.ResumeLayout(false);
             this.menuTitlePanel.ResumeLayout(false);
+            this.menuTitlePanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -218,6 +226,7 @@
         private System.Windows.Forms.Panel desktopPanel;
         private System.Windows.Forms.ImageList types;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
